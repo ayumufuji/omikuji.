379f9383c694b647ea46fb6a5757901dce976078
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-07-25 17:48:18 +0900

    kuji commit
    これは練習です。

---
+++ omikuji/js/index.js
@@ -0,0 +1,13 @@
+// ボタンクリック時にくじを引く
+function drawKuji() {
+   // ランダムな整数を作成（1-6の例）
+   let randomNumber = Math.floor(Math.random() * 6 + 1);
+
+   // くじの結果画像を表示させる
+   document.getElementsByClassName("kuji")[0].setAttribute('src', `images/kuji${randomNumber}.png`);　// バックティック文字で囲む事に注意(` `)
+
+   // ボタンの文字を変更する
+   document.querySelectorAll("button")[0].innerHTML = "もう一度引く";
+
+  document.querySelectorAll("h1")[0].innerHTML = "結果";
+}
